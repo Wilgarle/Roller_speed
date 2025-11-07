a7d3e05cf6b2d319050ad4da358064c2ac9188f0
wilgarle <william.garcial@est.iudigital.edu.co>
2025-11-06 19:14:21 -0500

    Modificacion del archivo V1 aplication java

--- v1/src/main/java/com/roller_speed_academy/v1/V1Application.java
+++ v1/src/main/java/com/roller_speed_academy/v1/V1Application.java
@@ -8,7 +8,7 @@ public class V1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(V1Application.class, args);
-		System.out.println("Roller Speed Academy V1 is running Correctly...");
+		System.out.println("Primer levantamiento del servicio");
 	}
 
 }
